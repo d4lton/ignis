d488e39c642c0d184ba5709e90172487b21c8d19
Dana Basken <dbasken@gmail.com>
2022-10-29 18:00:03 -0400

    add project support

--- src/Ignis.js
+++ src/Ignis.js
@@ -11,24 +11,45 @@ const Firestore = require("./firebase/Firestore");
 
 class Ignis {
 
-  config = new UserConfig("com.basken.ignis");
+  _config = new UserConfig("com.basken.ignis");
+  _firebases = {};
 
   constructor(argv) {
     this._argv = argv;
+    this._project = this._argv.project || this._config.get("project") || "default";
+  }
+
+
+  login() {
+    const firebaseConfig = this._config.get("firebase_config");
+    this._firebase = undefined;
+    this._firestore = undefined;
+    if (firebaseConfig) {
+      if (!this._firebases[this._project]) {
+        this._firebases[this._project] = new Firebase(this._config, this._project);
+      }
+      this._firebase = this._firebases[this._project];
+      this._firestore = new Firestore(this._firebase);
+    }
+  }
+
+  prompt(input) {
+    const loginIndicator = this._firebase ? "*" : "";
+    input.setPrompt(`${this._project}${loginIndicator} > `);
+    input.prompt();
   }
 
   async start() {
-    const input = readline.createInterface({input: process.stdin, output: process.stdout});
-
-    const firebaseConfig = this.config.get("firebase_config");
-    if (firebaseConfig) {
-      this._firebase = new Firebase(this.config);
-      this._firestore = new Firestore(this._firebase);
+    try {
+      this.login();
+    } catch (error) {
+      console.log(error.message);
     }
-
-    input.prompt();
+    const input = readline.createInterface({input: process.stdin, output: process.stdout});
+    this.prompt(input);
     input.on("line", async (line) => {
       const args = minimist(line.split(" "));
+      args._ = args._.filter(it => it);
       try {
         if (args._.length > 0) {
           switch (args._[0]) {
@@ -44,6 +65,12 @@ class Ignis {
             case "ls":
               await this.handleLsCommand(args);
               break;
+            case "project":
+              await this.handleProjectCommand(args);
+              break;
+            case "projects":
+              await this.handleProjectsCommand(args);
+              break;
             case "help":
               await this.handleHelpCommand(args);
               break;
@@ -55,7 +82,7 @@ class Ignis {
       } catch (error) {
         console.log(error.message);
       }
-      input.prompt();
+      this.prompt(input);
     });
 
   }
@@ -98,9 +125,9 @@ class Ignis {
     if (args.file) {
       if (fs.existsSync(args.file)) {
         const json = JSON.parse(fs.readFileSync(args.file).toString());
-        this.config.set("firebase_config", json);
-        this._firebase = new Firebase(this.config);
-        this._firestore = new Firestore(this._firebase);
+        if (args.project) { this._project = args.project; }
+        this._config.set(`firebase_config.${this._project}`, json);
+        this.login();
       } else {
         console.log(`Could not find file ${args.file}.`);
       }
@@ -144,11 +171,51 @@ class Ignis {
       if (args.out) {
         fs.writeFileSync(args.out, JSON.stringify(data));
       } else {
-        console.log(JSON.stringify(data));
+        for (const path of data) {
+          console.log(path);
+        }
       }
     } else {
       console.log("usage: ls <collection> [--out=<filename>]")
     }
+  }
+
+  async handleProjectCommand(args) {
+    if (args._.length === 1) {
+      console.log(`project: ${this._project}`);
+    } else if (args._.length === 2) {
+      this._project = args._[1];
+      this._config.set("project", this._project);
+      this.login();
+    } else {
+      console.log("usage: project [<project-id>] [--flush]")
+    }
+  }
+
+  async handleProjectsCommand(args) {
+    if (args.flush) {
+      await this.flush();
+    } else {
+      const firebaseConfig = this._config.get("firebase_config", {});
+      const projects = Object.keys(firebaseConfig);
+      if (projects.length) {
+        for (const project of projects) {
+          console.log(project);
+        }
+      } else {
+        console.log("No projects found.");
+      }
+    }
+  }
+
+  async flush() {
+    this._config.set("firebase_config", {});
+    for (const firebase of Object.values(this._firebases)) {
+      firebase.app.delete();
+    }
+    this._firebases = {};
+    console.log("All projects removed.");
+    this.login();
   }
 
 }
--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -5,9 +5,15 @@
 const admin = require("firebase-admin");
 
 class Firebase {
-  constructor(config) {
+
+  constructor(config, project) {
     this._config = config;
-    this._app = admin.initializeApp({credential: admin.credential.cert(this._config.get("firebase_config"))});
+    this._project = project;
+    const firebaseConfig = this._config.get("firebase_config");
+    if (!firebaseConfig) { throw new Error("No Firebase configurations found."); }
+    const projectConfig = firebaseConfig[project];
+    if (!projectConfig) { throw new Error(`No Firebase configuration found for project "${this._project}"`); }
+    this._app = admin.initializeApp({credential: admin.credential.cert(projectConfig)}, this._project);
   }
 
   get app() {
