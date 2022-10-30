e97262930c54b2fc6a1c1754713788bb37e0be10
Dana Basken <dbasken@gmail.com>
2022-10-30 09:59:13 -0400

    only try to read config.json if it exists

--- src/UserConfig.js
+++ src/UserConfig.js
@@ -10,6 +10,8 @@ const {ObjectUtilities} = require("@d4lton/utilities");
 
 class UserConfig {
 
+  _entries = {};
+
   constructor(key) {
     this._key = key;
     this._configPath = path.join(os.homedir(), ".config", this._key);
@@ -17,7 +19,9 @@ class UserConfig {
     if (!fs.existsSync(this._configPath)) {
       mkdirp.sync(this._configPath);
     }
-    this._entries = JSON.parse(fs.readFileSync(this._configFile).toString());
+    if (fs.existsSync(this._configFile)) {
+      this._entries = JSON.parse(fs.readFileSync(this._configFile).toString());
+    }
   }
 
   get(key, defaultValue = undefined) {
