732672295522e5dfdf657c593f2394292710de0c
Dana Basken <dbasken@gmail.com>
2024-10-06 12:13:38 -0400

    switch from 'temp' module to 'tmp' to avoid deprecated dependencies

--- src/commands/EditCommand.js
+++ src/commands/EditCommand.js
@@ -4,7 +4,7 @@
 
 const fs = require("node:fs");
 const {execSync}  = require("node:child_process");
-const temp = require("temp");
+const tmp = require("tmp");
 const Command = require("./Command");
 
 class EditCommand extends Command {
@@ -37,11 +37,11 @@ class EditCommand extends Command {
     let data = await projectPathInfo.firebase.firestore.get(projectPathInfo.path);
     if (!data) { data = {}; }
     data = JSON.stringify(data, null, 2);
-    const filename = temp.path({suffix: ".json"});
-    fs.writeFileSync(filename, data);
-    execSync(`${editor} ${filename}`, {stdio: "inherit"});
-    const updatedData = fs.readFileSync(filename).toString();
-    fs.unlinkSync(filename);
+    const temporaryFile = tmp.fileSync({postfix: ".json"});
+    fs.writeFileSync(temporaryFile.name, data);
+    execSync(`${editor} ${temporaryFile.name}`, {stdio: "inherit"});
+    const updatedData = fs.readFileSync(temporaryFile.name).toString();
+    temporaryFile.removeCallback();
     if (data !== updatedData) {
       await projectPathInfo.firebase.firestore.put(projectPathInfo.path, updatedData, args.merge);
     } else {
